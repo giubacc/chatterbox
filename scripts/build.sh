c27d45e82dab890045de9abdbe003b910843634c
Giuseppe Baccini <giuseppe.baccini@suse.com>
2022-11-15 14:28:15 +0100

    updated submodules
    Signed-off-by: Giuseppe Baccini <giuseppe.baccini@suse.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,12 +14,12 @@ commands
   clean                     Clean chatterbox build directory.
   build-all                 Build chatterbox with all its dependencies.
   clean-all                 Clean all build directories (chatterbox and dependencies).
+  build-deps                Build chatterbox's dependencies.
+  clean-deps                Clean chatterbox's dependencies.
   build-v8                  Build V8 JavaScript and WebAssembly engine.
   clean-v8                  Clean V8 JavaScript and WebAssembly engine.
   builder-create            Create the chatterbox builder.
   builder-build             Build chatterbox with the chatterbox builder.
-  builder-build-all         Build chatterbox with all its dependencies with the chatterbox builder.
-  builder-build-v8          Build V8 JavaScript and WebAssembly engine with the chatterbox builder.
   help                      This message.
 
 env variables
@@ -52,30 +52,6 @@ builder_build() {
     chatterbox-builder-${base_build_image} build || exit 1
 }
 
-builder_build_all() {
-  echo "Building chatterbox with all its dependencies with the chatterbox builder ..."
-  volumes="-v $basedir:/project"
-
-  podman run -it \
-    -e BASE_DIR=/project \
-    -e NINJA_JOBS=${ninja_jobs} \
-    -e CONTRIB_PATH="/contrib" \
-    ${volumes[@]} \
-    chatterbox-builder-${base_build_image} build-all || exit 1
-}
-
-builder_build_v8() {
-  echo "Building V8 JavaScript and WebAssembly engine with the chatterbox builder ..."
-  volumes="-v $basedir:/project"
-
-  podman run -it \
-    -e BASE_DIR=/project \
-    -e NINJA_JOBS=${ninja_jobs} \
-    -e CONTRIB_PATH="/contrib" \
-    ${volumes[@]} \
-    chatterbox-builder-${base_build_image} build-v8 || exit 1
-}
-
 build() {
   echo "Building chatterbox ..."
   if [[ -n "${contrib_path}" ]]; then
@@ -87,6 +63,36 @@ build() {
 clean() {
   echo "Cleaning chatterbox build ..."
   rm -rf $basedir/build
+}
+
+build_cryptopp() {
+  echo "Building cryptopp ..."
+  cd $contrib_path/cryptopp && make -f GNUmakefile
+}
+
+build_restclient_cpp() {
+  echo "Building restclient-cpp ..."
+  cd $contrib_path/restclient-cpp && ./autogen.sh && ./configure && make
+}
+
+build_jsoncpp() {
+  echo "Building jsoncpp-build ..."
+  mkdir -p $contrib_path/jsoncpp-build && cd $contrib_path/jsoncpp-build && cmake ../jsoncpp && make
+}
+
+clean_cryptopp() {
+  echo "Cleaning cryptopp ..."
+  cd $contrib_path/cryptopp && make -f GNUmakefile clean
+}
+
+clean_restclient_cpp() {
+  echo "Cleaning restclient-cpp ..."
+  cd $contrib_path/restclient-cpp && make clean
+}
+
+clean_jsoncpp() {
+  echo "Cleaning jsoncpp-build ..."
+  rm -rf $contrib_path/jsoncpp-build
 }
 
 # see this issue: https://bugs.chromium.org/p/v8/issues/detail?id=13455
@@ -123,31 +129,27 @@ clean_v8() {
   rm -rf $contrib_path/v8/out
 }
 
-build_all() {
-  echo "Building cryptopp ..."
-  cd $contrib_path/cryptopp && make -f GNUmakefile
-
-  echo "Building restclient-cpp ..."
-  cd $contrib_path/restclient-cpp && ./autogen.sh && ./configure && make
-
-  echo "Building jsoncpp-build ..."
-  mkdir -p $contrib_path/jsoncpp-build && cd $contrib_path/jsoncpp-build && cmake ../jsoncpp && make
-
+build_deps() {
+  build_cryptopp
+  build_restclient_cpp
+  build_jsoncpp
   build_v8
+}
+
+clean_deps() {
+  clean_cryptopp
+  clean_restclient_cpp
+  clean_jsoncpp
+  clean_v8
+}
+
+build_all() {
+  build_deps
   build
 }
 
 clean_all() {
-  echo "Cleaning cryptopp ..."
-  cd $contrib_path/cryptopp && make -f GNUmakefile clean
-
-  echo "Cleaning restclient-cpp ..."
-  cd $contrib_path/restclient-cpp && make clean
-
-  echo "Cleaning jsoncpp-build ..."
-  rm -rf $contrib_path/jsoncpp-build
-
-  clean_v8
+  clean_deps
   clean
 }
 
@@ -172,12 +174,6 @@ case ${cmd} in
   builder-build)
     builder_build || exit 1
     ;;
-  builder-build-all)
-    builder_build_all || exit 1
-    ;;
-  builder-build-v8)
-    builder_build_v8 || exit 1
-    ;;
   build)
     build || exit 1
     ;;
@@ -189,6 +185,12 @@ case ${cmd} in
     ;;
   clean-all)
     clean_all || exit 1
+    ;;
+  build-deps)
+    build_deps || exit 1
+    ;;
+  clean-deps)
+    clean_deps || exit 1
     ;;
   build-v8)
     build_v8 || exit 1
---
+++ scripts/contrib_init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+contrib_path=$(realpath ${CONTRIB_PATH:-"/contrib"})
+
+mkdir -p $contrib_path
+
+echo "Cloning contrib resources ..."
+
+cd $contrib_path
+git clone https://github.com/open-source-parsers/jsoncpp.git
+git clone https://github.com/weidai11/cryptopp.git
+git clone https://github.com/gabime/spdlog.git
+git clone https://github.com/muellan/clipp.git
+git clone https://github.com/giubacc/restclient-cpp.git
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+
+echo "Bumping contrib resources ..."
+
+cd $contrib_path/clipp && git checkout v1.2.3
+cd $contrib_path/jsoncpp && git checkout 1.9.5
+cd $contrib_path/restclient-cpp && git checkout set-verify-host
+cd $contrib_path/spdlog && git checkout v1.9.2
+cd $contrib_path/cryptopp && git checkout CRYPTOPP_8_7_0
