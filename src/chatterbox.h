b91b0d72ca38f481519ecf338b464e5cea16505e
Giuseppe Baccini <giuseppe.baccini@suse.com>
2022-12-09 17:30:44 +0100

    embedder's objects in javascript context and refactoring
    Signed-off-by: Giuseppe Baccini <giuseppe.baccini@suse.com>

--- src/chatterbox.h
+++ src/chatterbox.h
@@ -163,101 +163,27 @@ struct chatterbox {
     void move_file(const char *filename);
     void rm_file(const char *filename);
 
-    template <typename T>
-    struct Converter;
-
-    // ------------------
-    // --- Evaluators ---
-    // ------------------
-
-    template <typename T>
-    std::optional<T> eval_as(const Json::Value &from,
-                             const char *key,
-                             const std::optional<T> default_value = std::nullopt) {
-      Json::Value val = from.get(key, Json::Value::null);
-      if(val) {
-        if(Converter<T>::isType(val)) {
-          //eval as primitive value
-          return Converter<T>::asType(val);
-        } else {
-          //eval as javascript function
-          Json::Value function = val.get("function", Json::Value::null);
-          if(!function || !function.isString()) {
-            event_log_->error("function name is not string type");
-            return std::nullopt;
-          }
-
-          Json::Value j_args = val.get("args", Json::Value::null);
-          std::vector<std::string> args;
-          if(j_args) {
-            if(!j_args.isArray()) {
-              event_log_->error("function args are not array type");
-              return std::nullopt;
-            }
-            for(uint32_t i = 0; i < j_args.size(); ++i) {
-              Json::Value arg = j_args[i];
-              if(!arg.isString()) {
-                event_log_->error("function arg is not string type");
-                return std::nullopt;
-              }
-              args.emplace_back(arg.asString());
-            }
-          }
-
-          //finally invoke the javascript function
-          T result;
-          std::string error;
-
-          bool js_res = js_env_.invoke_js_function(function.asCString(),
-                                                   args,
-          [&](v8::Isolate *isl, const std::vector<std::string> &args, v8::Local<v8::Value> argv[]) -> bool{
-            std::transform(args.begin(), args.end(), argv, [&](const std::string &it) -> v8::Local<v8::String> {
-              return v8::String::NewFromUtf8(isl, it.c_str(), v8::NewStringType::kNormal).ToLocalChecked();
-            });
-            return true;
-          },
-          [&](v8::Isolate *isl, const v8::Local<v8::Value> &res) -> bool{
-            if(!Converter<T>::isType(res)) {
-              std::stringstream ss;
-              ss << "function result is not " << Converter<T>::name() << " type";
-              event_log_->error(ss.str());
-              return false;
-            }
-            result = Converter<T>::asType(res, isl);
-            return true;
-          },
-          error);
-
-          if(!js_res) {
-            event_log_->error("failure invoking function:{}, error:{}", function.asString(), error);
-            return std::nullopt;
-          }
-          return result;
-        }
-      } else {
-        return default_value;
-      }
-    }
-
-
-    // ----------------
-    // --- JS Utils ---
-    // ----------------
-
-    bool exec_as_function(const Json::Value &from,
-                          const char *key,
-                          bool optional = true);
-
   public:
     cfg cfg_;
+
+    //current scenario_in and scenario_out
+    Json::Value scenario_in_;
+    Json::Value scenario_out_;
+    std::stack<utils::json_value_ref> stack_obj_in_;
+    std::stack<utils::json_value_ref> stack_obj_out_;
+
+    //conversation statistics
+    uint32_t conv_request_count_ = 0;
+    std::unordered_map<std::string, int32_t> conv_res_code_categorization_;
+
+    //scenario statistics
+    uint32_t scen_conversation_count_ = 0;
+    uint32_t scen_request_count_ = 0;
+    std::unordered_map<std::string, int32_t> scen_res_code_categorization_;
 
   private:
     //conversation connection
     std::unique_ptr<RestClient::Connection> conv_conn_;
-
-    //current scenario and scenario_out
-    Json::Value scenario_;
-    Json::Value scenario_out_;
 
     //conversation context
     std::string access_key_;
@@ -270,15 +196,6 @@ struct chatterbox {
     std::string service_;
     bool res_conv_dump_ = true;
 
-    //conversation statistics
-    uint32_t conv_request_count_ = 0;
-    std::unordered_map<std::string, int32_t> conv_res_code_categorization_;
-
-    //scenario statistics
-    uint32_t scen_conversation_count_ = 0;
-    uint32_t scen_request_count_ = 0;
-    std::unordered_map<std::string, int32_t> scen_res_code_categorization_;
-
     //js environment
     js::js_env js_env_;
 
@@ -288,64 +205,6 @@ struct chatterbox {
   public:
     std::string event_log_fmt_;
     std::shared_ptr<spdlog::logger> event_log_;
-};
-
-template <>
-struct chatterbox::Converter<bool> {
-  static bool isType(const Json::Value &val) {
-    return val.isBool();
-  }
-  static bool isType(const v8::Local<v8::Value> &val) {
-    return val->IsBoolean();
-  }
-  static bool asType(const Json::Value &val) {
-    return val.asBool();
-  }
-  static bool asType(const v8::Local<v8::Value> &val, v8::Isolate *isl) {
-    return val->BooleanValue(isl);
-  }
-  static std::string name() {
-    return "boolean";
-  }
-};
-
-template <>
-struct chatterbox::Converter<uint32_t> {
-  static bool isType(const Json::Value &val) {
-    return val.isUInt();
-  }
-  static bool isType(const v8::Local<v8::Value> &val) {
-    return val->IsUint32();
-  }
-  static uint32_t asType(const Json::Value &val) {
-    return val.asUInt();
-  }
-  static uint32_t asType(const v8::Local<v8::Value> &val, v8::Isolate *isl) {
-    return val->Uint32Value(isl->GetCurrentContext()).FromMaybe(0);
-  }
-  static std::string name() {
-    return "uint32_t";
-  }
-};
-
-template <>
-struct chatterbox::Converter<std::string> {
-  static bool isType(const Json::Value &val) {
-    return val.isString();
-  }
-  static bool isType(const v8::Local<v8::Value> &val) {
-    return val->IsString();
-  }
-  static std::string asType(const Json::Value &val) {
-    return val.asString();
-  }
-  static std::string asType(const v8::Local<v8::Value> &val, v8::Isolate *isl) {
-    v8::String::Utf8Value utf_res(isl, val);
-    return *utf_res;
-  }
-  static std::string name() {
-    return "string";
-  }
 };
 
 }
--- src/globals.h
+++ src/globals.h
@@ -109,4 +109,9 @@ int read_file(const char *fname,
               std::stringstream &out,
               spdlog::logger *log);
 
+struct json_value_ref {
+  json_value_ref(Json::Value &ref) : ref_(ref) {}
+  Json::Value &ref_;
+};
+
 }
